9f9fe9922807cf1be0cabcff76551164ac14801a
Vlad Golubev <holybay1886@gmail.com>
2024-11-15 19:18:19 +0300

    Replaced DateUtil class with custom String to LocalDateTime converter impl class.

--- src/main/java/by/it_academy/jd2/finance/libs/shared_lib/util/LocalDateTimeConverterFromLong.java
+++ src/main/java/by/it_academy/jd2/finance/libs/shared_lib/util/LocalDateTimeConverterFromLong.java
@@ -1,12 +1,16 @@
 package by.it_academy.jd2.finance.libs.shared_lib.util;
 
+import org.springframework.core.convert.converter.Converter;
+
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-public class DateUtil {
+public class LocalDateTimeConverterFromLong implements Converter<String, LocalDateTime> {
 
-    public static LocalDateTime longToLocalDateTime(long epochTime) {
+    @Override
+    public LocalDateTime convert(String source) {
+        long epochTime = Long.parseLong(source);
         Instant instant = Instant.ofEpochMilli(epochTime);
         return LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
     }
