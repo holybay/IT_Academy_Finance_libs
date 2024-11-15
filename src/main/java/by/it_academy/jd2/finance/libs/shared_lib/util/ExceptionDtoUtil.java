263d2681e988fef53ba11fae71cb0e3095a4bb73
Vlad Golubev <holybay1886@gmail.com>
2024-11-15 12:57:45 +0300

    Split utils into 2 separate classes.

--- src/main/java/by/it_academy/jd2/finance/libs/shared_lib/util/ExceptionDtoUtil.java
+++ src/main/java/by/it_academy/jd2/finance/libs/shared_lib/util/ExceptionDtoUtil.java
@@ -4,18 +4,10 @@ import by.it_academy.jd2.finance.libs.shared_lib.exception.ApplicationStructured
 import by.it_academy.jd2.finance.libs.shared_lib.exception.dto.AppExceptionDtoSimple;
 import by.it_academy.jd2.finance.libs.shared_lib.exception.dto.StructuredExceptionDto;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Util {
-
-    public static LocalDateTime longToLocalDateTime(long epochTime) {
-        Instant instant = Instant.ofEpochMilli(epochTime);
-        return LocalDateTime.ofInstant(instant, ZoneOffset.UTC);
-    }
+public class ExceptionDtoUtil {
 
     public static List<AppExceptionDtoSimple> getAppExceptionSimpleDto(Exception e) {
         List<AppExceptionDtoSimple> errors = new ArrayList<>();
